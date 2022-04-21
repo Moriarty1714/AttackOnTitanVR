0ebad285ea99c8b89fad6caf9bbffbd7b3f84f72
Moriarty <marti.deferrer@enti.cat>
2022-04-21 20:32:48 +0200

    RaycastHit implemente + DrawLine

---
+++ Assets/Proba.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+public class Proba : MonoBehaviour
+{
+    [SerializeField] GameObject hitGameObject;
+    LineRenderer myLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLine = GetComponent<LineRenderer>();
+        myLine.startWidth = 0.02f;
+        myLine.endWidth = 0.02f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 100f, Color.cyan);
+        myLine.SetPosition(0, transform.position);
+        myLine.SetPosition(1, transform.position + transform.forward * 100f);
+
+        RaycastHit hit;
+        
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 100f))
+        {
+            Debug.Log("Estoy dentro");
+            hitGameObject.transform.position = hit.point;
+        }
+    }
+}
