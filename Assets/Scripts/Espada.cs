19c906031feca49c534e1cfb59f48f617f2552cc
Diddylcp <borja.tapia@enti.cat>
2022-05-12 19:58:44 +0200

    Cambios tamañao espada

--- Assets/Scripts/Espada.cs
+++ Assets/Scripts/Espada.cs
@@ -35,7 +35,7 @@ public class Espada : MonoBehaviour
 
     [SerializeField]
     [Tooltip("The amount of force applied to each side of a slice")]
-    private float _forceAppliedToCut = 3f;
+    private float _forceAppliedToCut = 300f;
 
     private Mesh _mesh;
     private Vector3[] _vertices;
@@ -119,51 +119,58 @@ public class Espada : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        _triggerEnterTipPosition = _tip.transform.position;
-        _triggerEnterBasePosition = _base.transform.position;
+        if (other.tag == "Sliceable")
+        {
+            _triggerEnterTipPosition = _tip.transform.position;
+            _triggerEnterBasePosition = _base.transform.position;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        _triggerExitTipPosition = _tip.transform.position;
-
-        //Create a triangle between the tip and base so that we can get the normal
-        Vector3 side1 = _triggerExitTipPosition - _triggerEnterTipPosition;
-        Vector3 side2 = _triggerExitTipPosition - _triggerEnterBasePosition;
-
-        //Get the point perpendicular to the triangle above which is the normal
-        //https://docs.unity3d.com/Manual/ComputingNormalPerpendicularVector.html
-        Vector3 normal = Vector3.Cross(side1, side2).normalized;
-
-        //Transform the normal so that it is aligned with the object we are slicing's transform.
-        Vector3 transformedNormal = ((Vector3)(other.gameObject.transform.localToWorldMatrix.transpose * normal)).normalized;
-
-        //Get the enter position relative to the object we're cutting's local transform
-        Vector3 transformedStartingPoint = other.gameObject.transform.InverseTransformPoint(_triggerEnterTipPosition);
-
-        Plane plane = new Plane();
-
-        plane.SetNormalAndPosition(
-                transformedNormal,
-                transformedStartingPoint);
-
-        var direction = Vector3.Dot(Vector3.up, transformedNormal);
-
-        //Flip the plane so that we always know which side the positive mesh is on
-        if (direction < 0)
+        if(other.tag == "Sliceable")
         {
-            plane = plane.flipped;
-        }
+            _triggerExitTipPosition = _tip.transform.position;
 
-        GameObject[] slices = Slicer.Slice(plane, other.gameObject);
-        foreach(GameObject slice in slices)
-        {
-            Destroy(slice.GetComponent<Sliceable>());
-        }
-        Destroy(other.gameObject);
+            //Create a triangle between the tip and base so that we can get the normal
+            Vector3 side1 = _triggerExitTipPosition - _triggerEnterTipPosition;
+            Vector3 side2 = _triggerExitTipPosition - _triggerEnterBasePosition;
 
-        Rigidbody rigidbody = slices[1].GetComponent<Rigidbody>();
-        Vector3 newNormal = transformedNormal + Vector3.up * _forceAppliedToCut;
-        rigidbody.AddForce(newNormal, ForceMode.Impulse);
+            //Get the point perpendicular to the triangle above which is the normal
+            //https://docs.unity3d.com/Manual/ComputingNormalPerpendicularVector.html
+            Vector3 normal = Vector3.Cross(side1, side2).normalized;
+
+            //Transform the normal so that it is aligned with the object we are slicing's transform.
+            Vector3 transformedNormal = ((Vector3)(other.gameObject.transform.localToWorldMatrix.transpose * normal)).normalized;
+
+            //Get the enter position relative to the object we're cutting's local transform
+            Vector3 transformedStartingPoint = other.gameObject.transform.InverseTransformPoint(_triggerEnterTipPosition);
+
+            Plane plane = new Plane();
+
+            plane.SetNormalAndPosition(
+                    transformedNormal,
+                    transformedStartingPoint);
+
+            var direction = Vector3.Dot(Vector3.up, transformedNormal);
+
+            //Flip the plane so that we always know which side the positive mesh is on
+            if (direction < 0)
+            {
+                plane = plane.flipped;
+            }
+
+            GameObject[] slices = Slicer.Slice(plane, other.gameObject);
+            foreach (GameObject slice in slices)
+            {
+                //Destroy(slice.GetComponent<Sliceable>());
+            }
+            Destroy(other.gameObject);
+
+            Rigidbody rigidbody = slices[1].GetComponent<Rigidbody>();
+            Vector3 newNormal = transformedNormal + (Vector3.up + side1.normalized).normalized * _forceAppliedToCut;
+            rigidbody.AddForce(newNormal, ForceMode.Impulse);
+        }
+        
     }
 }
