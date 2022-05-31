b018873a2ab974515c20608a06c0474a8ddfae46
Martí <marti.deferrer@enti.cat>
2022-05-31 22:40:34 +0200

    Trees done

--- Assets/Sequoia_OBJ/RandomRot.cs
+++ Assets/Sequoia_OBJ/RandomRot.cs
@@ -8,6 +8,8 @@ public class RandomRot : MonoBehaviour
     void Start()
     {
         transform.Rotate(new Vector3(0, 1, 0), Random.Range(0, 360));
-        transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        transform.Rotate(new Vector3(1,0,0), -90);
+        float tmpScale = Random.Range(8.0f, 12.0f);
+        transform.localScale = new Vector3(tmpScale, tmpScale, tmpScale);
     }
 }
